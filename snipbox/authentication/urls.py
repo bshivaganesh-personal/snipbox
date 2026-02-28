c06f58a7a42aa39c07182971fe3c1aed21be05a2
shivaganesh1404 <shivaganesh1404@gmail.com>
2026-02-28 06:21:28 +0000

    feat: authentication login and refresh class

--- snipbox/authentication/urls.py
+++ snipbox/authentication/urls.py
@@ -1,7 +1,10 @@
 """Authentication URL patterns for SnipBox."""
 
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import CustomLoginView
 
 urlpatterns = [
+    path("login/", CustomLoginView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- snipbox/authentication/views.py
+++ snipbox/authentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""Custom authentication views for SnipBox."""
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """Extend the default serializer to include user info in response."""
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Add extra user info to the response
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email,
+        }
+        return data
+
+
+class CustomLoginView(TokenObtainPairView):
+    """Custom login view returning tokens + user details."""
+
+    serializer_class = CustomTokenObtainPairSerializer
